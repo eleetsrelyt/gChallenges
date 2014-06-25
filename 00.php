47364e7346878bdf31b9c5a2e38fc34ab79a0161
Tyler Steele <steeletyler@gmail.com>
2014-06-25 12:23:34 -0600

    button functionality added
    button reloads page and updates name but doesn't initiate the name by
    itself.

--- 00.php
+++ 00.php
@@ -6,31 +6,34 @@ $cs = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','
 //echo rand(0,20);
 //echo $vs[rand(0,count($vs)-1)];
 //echo $cs[rand(0,count($cs)-1)];
+
+
 $name = '';
-
-
-$len = rand(3,8);
+$len = rand(3,7);
 //echo "<br/>" . $len. "<br/>";
 
 if (isset($_POST['GENERATE'])) {
-	for ( $i = 0 ; $i <= $len ; $i++) {
-		$j = rand(1,3);
-		//echo $j . "<br/>";
-		if ($j == 1) {
-			$name = $name . $vs[rand(0,count($vs)-1)];
-		}
-		else {
-			$name = $name . $cs[rand(0,count($cs)-1)];
-		}
-	}
+	//echo "button test";
 }
 
+
+for ( $i = 0 ; $i < $len ; $i++) {
+	$j = rand(1,3);
+	//echo $j . "<br/>";
+	if ($j == 1) {
+		$name = $name . $vs[rand(0,count($vs)-1)];
+	}
+	else {
+		$name = $name . $cs[rand(0,count($cs)-1)];
+	}
+}
 
 echo <<<_END
 <html><head></head>
 <body>
 <pre>
-	<input type="submit" value="GENERATE">
+<form method="post" action="00.php">
+	<input type="submit" value="GENERATE"></form>
 		$name
 </pre></body></html>
 _END;

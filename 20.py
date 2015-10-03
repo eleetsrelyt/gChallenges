36eee560ec3ad9a6c15ecde370a79a536dbf56e4
Tyler Steele <steele.tyler@gmail.com>
2015-10-02 22:41:18 -0600

    Finished exercise 20
    I finished exercise 20 in python. I’ll add input for feet and inches
    later

--- 20.py
+++ 20.py
@@ -1,5 +1,20 @@
-print("BMI Calculator")
-print("--------------")
-height = input("Enter your height in the format feet,inches: ")
-weight = input("Enter your weight in pounds: ")
-heightIn = 
+print("       BMI Calculator       ")
+print("----------------------------")
+height_in = float(input("Enter your height in inches: "))
+weight_lbs = float(input("Enter your weight in pounds: "))
+
+weight_kg = weight_lbs * 0.45
+height_m = height_in * 0.025
+
+height_2 = height_m * height_m
+bmi = round(weight_kg / height_2,1)
+
+if bmi<19:
+    health = ", which is low."
+else:
+    if bmi>25:
+        health = ", which is high."
+    else:
+        health = ", which is healthy."
+
+print("Your BMI is",str(bmi) + health)

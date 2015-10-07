a2eebf8cdf29af933b1e83059d5bbd0b3c4bb96c
Tyler Steele <steeletyler@gmail.com>
2015-10-06 20:20:56 -0600

    Calculate age in seconds
    I’m making this challenge a bit more refined. It’s still a work in
    progress.

--- 03.py
+++ 03.py
@@ -1,8 +1,14 @@
 ## Calculate age in seconds
 
+print(time())
+print(times())
+
 print("Calculate your age in seconds!")
-age = float(input("What is your age?"))
-
-ageSeconds = age * 365.25 * 24 * 60 * 60
-
-print("Your age in seconds is",ageSeconds)
+year = input("What year were you born? (YYYY) ")
+if len(str(year)) == 2:
+    yearStr = str(year)
+    q = str(input("You were born in 19" + yearStr + "? (y/n)"))
+    if q == "n":
+        year = input("What year were you born? (YYYY) ")
+month = input("What month were you born? (MM) ")
+day = input("What day where you born? (DD) ")

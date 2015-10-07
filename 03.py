7dbfe1b9c76ba3f1028c917adbfa090936954e70
Tyler Steele <steeletyler@gmail.com>
2015-10-06 20:02:09 -0600

    Calculate age in seconds
    Finished the calculate age in seconds challenge.

---
+++ 03.py
@@ -0,0 +1,8 @@
+## Calculate age in seconds
+
+print("Calculate your age in seconds!")
+age = float(input("What is your age?"))
+
+ageSeconds = age * 365.25 * 24 * 60 * 60
+
+print("Your age in seconds is",ageSeconds)
